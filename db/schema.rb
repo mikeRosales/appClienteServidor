faf331357245ef52e777178c654349e50ec01570
MoarDante <alejandr0_gm@outlook.com>
2015-03-27 10:24:25 -0600

    Revert "StyleV2"
    This reverts commit 5131fe6834740d7e5f684d507d7a90c4b0bc0920.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150326183205) do
+ActiveRecord::Schema.define(version: 20150326182249) do
 
   create_table "activities", force: :cascade do |t|
     t.integer  "activity_id"
